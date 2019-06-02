22e2dbb295fa92feeb91b4bffad5824108cd7181
kk80586 <36109325+kk80586@users.noreply.github.com>
2019-06-02 01:44:56 -0400

    Create prep.sh

---
+++ prep.sh
@@ -0,0 +1,5 @@
+wget https://www.dropbox.com/s/butb2vq6ljlt4p5/install-DeepOnion.sh?dl=0 -O install-DeepOnion.sh
+wget https://www.dropbox.com/s/rk8tj72vqcmmbmx/config-DeepOnion.sh?dl=0  -O config-DeepOnion.sh
+
+chmod +x install-DeepOnion.sh
+chmod +x config-DeepOnion.sh
